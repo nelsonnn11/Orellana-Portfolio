9847e95ed79803ed46db8cbd2fef634773bd232c
124norellana@frhsd.com <124norellana@frhsd.com>
2024-05-17 08:35:31 -0400

    New Portfolio

---
+++ main.js
@@ -0,0 +1,11 @@
+// main.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const navLinks = document.getElementById('nav-links');
+  
+    menuIcon.addEventListener('click', function () {
+      navLinks.classList.toggle('active');
+    });
+  });
+  
