2e43b784d9df75630e9ebe359a9fbd3f5782e0ad
124norellana@frhsd.com <124norellana@frhsd.com>
2024-05-22 08:36:32 -0400

    HOME PAGE COMPLETE

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 // main.js
 
 document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.getElementById('menu-icon');
-    const navLinks = document.getElementById('nav-links');
-  
-    menuIcon.addEventListener('click', function () {
-      navLinks.classList.toggle('active');
+  const menuIcon = document.getElementById('menu-icon');
+  const navLinks = document.getElementById('nav-links');
+
+  menuIcon.addEventListener('click', function () {
+    navLinks.classList.toggle('active');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navLinks = document.querySelectorAll('nav a');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      const targetId = event.currentTarget.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        event.preventDefault();
+        window.scrollTo({
+          top: targetSection.offsetTop,
+          behavior: 'smooth'
+        });
+      }
     });
   });
-  
+});
